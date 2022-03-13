b61994955bd769f4f94e9a19d349e8d134d99e2b
Nathandilan <100848038+Nathandilan@users.noreply.github.com>
2022-03-13 20:24:21 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"💀KARMAN-USERBOT💀 V9.0 [ TELAH DIAKTIFKAN KONTOL]")
+    f"stitch - userbot V9.0 [ TELAH DIAKTIFKAN KONTOL]")
 
 
 if len(argv) not in (1, 3, 4):
